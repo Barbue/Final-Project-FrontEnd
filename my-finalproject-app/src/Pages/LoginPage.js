83016e7a6ea5e27be493d023c342d0a7152b54e5
Barbue <david.crochet@codeimmersives.com>
2023-04-19 16:29:57 -0500

    modified login/registration

--- my-finalproject-app/src/Pages/LoginPage.js
+++ my-finalproject-app/src/Pages/LoginPage.js
@@ -1,51 +1,119 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../Hooks/Auth";
+import { useAuth } from "../Hooks/AuthContext";
 
-const LoginPage = (props) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loginMessage, setLoginMessage] = useState("");
-  const auth = useAuth(); //access the authentication context 
-	const navigate = useNavigate() // be able to navigate to home on login
+export default function Login() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const { login } = useAuth();
 
-  return (
-    <div>
-      <h1>Login Page</h1>
-      <h3>{loginMessage}</h3>
-      <label>Email: </label>
-      <input
-        type="text"
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-      />
-      <label> Password: </label>
-      <input
-        type="password"
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-      />
+	const navigate = useNavigate();
+
+	const handleSubmit = async () => {
+		const userData = {
+			email: email,
+			password: password,
+		};
+		const loginResult = await login(userData);
+		if (loginResult) {
+			navigate("/");
+		} else {
+			navigate("/register");
+		}
+	};
+	return (
+		<div>
+			<h1>Login Page</h1>
+			<label htmlFor="email">Email:</label>
+			<input
+				type="text"
+				name="email"
+				value={email}
+				onChange={(e) => setEmail(e.target.value)}
+			/>
+			<br />
+			<label htmlFor="password">Password:</label>
+			<input
+				type="text"
+				name="password"
+				value={password}
+				onChange={(e) => setPassword(e.target.value)}
+			/>
+			<button onClick={handleSubmit}>Login</button>
+		</div>
+	);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const LoginPage = (props) => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   console.log(useAuth());
+
+//   const [loginMessage, setLoginMessage] = useState("");
+//   const auth = useAuth(); //access the authentication context 
+// 	const navigate = useNavigate() // be able to navigate to home on login
+
+//   return (
+//     <div>
+//       <h1>Login Page</h1>
+//       <h3>{loginMessage}</h3>
+//       <label>Email: </label>
+//       <input
+//         type="text"
+//         onChange={(e) => {
+//           setEmail(e.target.value);
+//         }}
+//       />
+//       <label> Password: </label>
+//       <input
+//         type="password"
+//         onChange={(e) => {
+//           setPassword(e.target.value);
+//         }}
+//       />
       
-      <button
-        onClick={async () => {
+//       <button
+//         onClick={async () => {
 
-          //login in using auth context
-          const loginResult = await auth.login(email, password);
-					console.log("button onclick loginResult: ", loginResult)
-					if (loginResult.success) {
-						navigate("/")
-					}
-          if (!loginResult.success) {
-            setLoginMessage(loginResult.message)
-          }
-        }}
-      >
-        Login
-      </button>
-    </div>
-  );
-};
+//           //login in using auth context
+//           const loginResult = await auth.login(email, password);
+// 					console.log("button onclick loginResult: ", loginResult)
+// 					if (loginResult.success) {
+// 						navigate("/")
+// 					}
+//           if (!loginResult.success) {
+//             setLoginMessage(loginResult.message)
+//           }
+//         }}
+//       >
+//         Login
+//       </button>
+//     </div>
+//   );
+// };
 
-export default LoginPage;
+// export default LoginPage;
--- my-finalproject-app/src/Pages/RegistrationPage.js
+++ my-finalproject-app/src/Pages/RegistrationPage.js
@@ -1,49 +1,113 @@
 import { useState } from "react";
-import { useAuth } from "../Hooks/Auth";
+import { registerUser } from "../Hooks/authUtils";
 import { useNavigate } from "react-router-dom";
 
-const RegistrationPage = (props) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [registerMessage, setRegisterMessage] = useState("");
+export default function Registration() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [role, setRole] = useState("user");
 
-  //we are accessing the authentication context from within our 
-  // component 
-  const auth = useAuth();
-  const navigate = useNavigate();
-  return (
-    <div>
-      <h1>Registration Page</h1>
-      <h3>{registerMessage}</h3>
-      <label>Email: </label>
-      <input
-        type="text"
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-      />
-      <label> Password:  </label>
-      <input
-        type="password"
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-      />
-      <button
-        onClick={async () => {
-          const registerResult = await auth.register(email, password);
-          if (registerResult.success) {
-						navigate("/login");
-          }
-          if (!registerResult.success) {
-            setRegisterMessage(registerResult.message);
-          }
-        }}
-      >
-        Signup
-      </button>
-    </div>
-  );
-};
+	const navigate = useNavigate();
 
-export default RegistrationPage;
+	const handleSubmit = async () => {
+		const newUser = {
+			email: email,
+			password: password,
+			role: role,
+		};
+		const registerResult = await registerUser(newUser);
+		if (registerResult) {
+			navigate("/");
+		}
+	};
+
+	return (
+		<div>
+			<h1>Registration Page</h1>
+			<label htmlFor="email">Email:</label>
+			<input
+				type="text"
+				name="email"
+				value={email}
+				onChange={(e) => setEmail(e.target.value)}
+			/>
+			<br />
+			<label htmlFor="password">Password:</label>
+			<input
+				type="text"
+				name="password"
+				value={password}
+				onChange={(e) => setPassword(e.target.value)}
+			/>
+			<button onClick={handleSubmit}>Register</button>
+		</div>
+	);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState } from "react";
+// import { useAuth } from "../Hooks/Auth";
+// import { useNavigate } from "react-router-dom";
+
+// const RegistrationPage = (props) => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [registerMessage, setRegisterMessage] = useState("");
+
+//   //we are accessing the authentication context from within our 
+//   // component 
+//   const auth = useAuth();
+//   const navigate = useNavigate();
+//   return (
+//     <div>
+//       <h1>Registration Page</h1>
+//       <h3>{registerMessage}</h3>
+//       <label>Email: </label>
+//       <input
+//         type="text"
+//         onChange={(e) => {
+//           setEmail(e.target.value);
+//         }}
+//       />
+//       <label> Password:  </label>
+//       <input
+//         type="password"
+//         onChange={(e) => {
+//           setPassword(e.target.value);
+//         }}
+//       />
+//       <button
+//         onClick={async () => {
+//           const registerResult = await auth.register(email, password);
+//           if (registerResult.success) {
+// 						navigate("/login");
+//           }
+//           if (!registerResult.success) {
+//             setRegisterMessage(registerResult.message);
+//           }
+//         }}
+//       >
+//         Signup
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default RegistrationPage;
