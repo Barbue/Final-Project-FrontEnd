a93a2572c9c6fb9ea61fcc0683009f4fc9a8f278
Barbue <david.crochet@codeimmersives.com>
2023-04-19 16:33:34 -0500

    modified to remove previous use of auth

--- my-finalproject-app/src/Pages/TicketsList.js
+++ my-finalproject-app/src/Pages/TicketsList.js
@@ -1,39 +1,95 @@
 //import { useState } from "react";
-
+//import { useAuth } from "../Hooks/Auth";
+//import { useNavigate  } from 'react-router-dom';
+//import Pagination from "../Components/Pagination";
 import TicketCard from "../Components/TicketCard";
-import { useAuth } from "../Hooks/Auth";
-import { useNavigate  } from 'react-router-dom';
+import SearchBar from '../Components/SearchBar'
+
 
 //home page component 
 const TicketsList = (props) => {
-    const auth = useAuth();
-    const navigate = useNavigate();
-
-    const {
-        ticketList, 
+    // const auth = useAuth();
+    // const navigate = useNavigate();
+const {
+        ticketList,   
         setTicketList, 
         urlEndPoint,
         setShouldRefresh, 
-    } = props
+        filterTickets,
+      } = props
     
     console.log(ticketList)
 
-    return (
+return (
         <div>
-            {!auth.userToken && navigate("/login")}
+            {/* {!auth.userToken && navigate("/login")} */}
+         <br/>
+            <SearchBar filterTickets={filterTickets}/>
             
-            {auth.userToken && <h1>Ticket App</h1> && ticketList.map((item, index) => {
-                return (<TicketCard 
-                    ticket={item} 
-                    setTicketList={setTicketList} 
-                    urlEndPoint={urlEndPoint}
-                    setShouldRefresh={setShouldRefresh}
-                    key={index} /> 
+            {/* <h1>Ticket App</h1> */}
+            
+            
+            {ticketList.map((item, index) => {
+            
+            
+            return (
+            
+              <TicketCard
+            ticket={item}
+            setTicketList={setTicketList}
+            urlEndPoint={urlEndPoint}
+            setShouldRefresh={setShouldRefresh}
+            key={index} />
                 );
             })}
+
+           
+
+           
+            
             
         </div>
     )
 }
 
 export default TicketsList
+
+
+
+// <Pagination 
+           
+        //    totalTickets={ticketList.length} 
+        //    ticketsPerPage={ticketsPerPage}
+        //    setCurrentPage={setCurrentPage}
+        //    />   
+
+
+
+
+    //        return (
+    //         <div>
+    //             {!auth.userToken && navigate("/login")}
+    
+    //             {auth.userToken && <SearchBar filterTickets={filterTickets}/>}
+                
+    //             {auth.userToken && <h1>Ticket App</h1>&& 
+    //             ticketList.map((item, index) => {
+    //     return (
+                
+    //               <TicketCard
+    //             ticket={item}
+    //             setTicketList={setTicketList}
+    //             urlEndPoint={urlEndPoint}
+    //             setShouldRefresh={setShouldRefresh}
+    //             key={index} />
+    //                 );
+    //             })}
+    
+               
+    
+               
+                
+                
+    //         </div>
+    //     )
+    // }
