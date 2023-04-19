f85fcb6772d26c17d385c60fd5a2d208ea47f223
Barbue <david.crochet@codeimmersives.com>
2023-04-19 16:23:26 -0500

    modified navbar for auth/verified

--- my-finalproject-app/src/Components/NavBar.js
+++ my-finalproject-app/src/Components/NavBar.js
@@ -1,31 +1,107 @@
-// import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useAuth } from "../Hooks/Auth";
+import { useAuth } from "../Hooks/AuthContext";
+import { removeUserToken } from "../Hooks/authLocalStorage";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
-    //we need this in the navbar, so that we can log out
-	//instead of passing logout through props,
-	// we are importing the useAuth which is Auth's 
-	// contxt which allows us to call logout()
- const auth = useAuth();
-    return (
-        <div>
-            <h3>{auth.userEmail && `Current User: ${auth.userEmail}`}</h3>
-            <Link className="link" to="/"> Home </Link>
-            <Link className="link5" to="/ticketslist"> Tickets list </Link>
-            <Link className="link2" to="/ticketform"> Ticket Form </Link> 
-            <Link className="link3" to="/registration"> Registration Form </Link>
-			<Link className="link4" to="/login"> Login Form</Link>
-            
-            
-            
-			<button onClick={()=>{
-				auth.logout()
-			}}>Logout</button>
-        </div>
+    
+  const {isVerified, logout} = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    const logoutResult = await logout();
+    if (logoutResult) navigate("/login");
+    
+};
+
+return (
+
+          <nav> 
+
+           <Link className="link" to="/"> Home </Link>   
+           
+           {isVerified && <Link className="link2" to="/ticketform"> Create Ticket </Link> }
+
+         {isVerified && <Link className="link3" to="/registration"> Register </Link>}
+
+          <Link className="link4" to="/login"> Login </Link>
+          
+         {isVerified && <button onClick={handleLogout}>Logout</button>}
+
+         
+          </nav> 
+       
+    // </div>
     )
-}
+ }
+
+
+
+
 export default NavBar;
 
-//"/registration"
-//"/login"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // import { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+// import { useAuth } from "../Hooks/Auth";
+
+// const NavBar = () => {
+//     //we need this in the navbar, so that we can log out
+// 	//instead of passing logout through props,
+// 	// we are importing the useAuth which is Auth's 
+// 	// contxt which allows us to call logout()
+// //  const auth = useAuth();
+//     return (
+//         //<div>
+//          <nav> 
+//             <Link className="link" to="/"> Home </Link>
+//             {/* <Link className="link5" to="/ticketslist"> Tickets list </Link> */}
+            
+//             <Link className="link2" to="/ticketform"> Create Ticket </Link> 
+
+            
+//             <Link className="link3" to="/registration"> Register </Link>
+           
+// 			<Link className="link4" to="/login"> Login </Link>
+
+//         {/* <button onClick={  }>Logout</button> */}
+//                  {/* ()=>{ auth.logout() } */}
+//          </nav> 
+       
+//       // </div>
+//     )
+// }
+// export default NavBar;
+
+// //"/registration"
+// //"/login"
+
+// <Link className="link5" to="/ticketslist"> Tickets list </Link> 
