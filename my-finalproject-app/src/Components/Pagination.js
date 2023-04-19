b8015cbdaa87ed5ae14ac31b9689d1c36c260529
Barbue <david.crochet@codeimmersives.com>
2023-04-19 13:11:55 -0500

    added pagination

---
+++ my-finalproject-app/src/Components/Pagination.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useState } from "react";
+
+
+const Pagination = ({totalTickets, ticketsPerPage, setCurrentPage}) => {
+    let pages = [];
+     
+    for(let i = 1; i <= Math.ceil(totalTickets / ticketsPerPage); i++)
+    {
+     pages.push(i)
+    }
+    
+
+  return (
+    <div>
+        {
+           pages.map((page, i) => {
+
+           return <button key={i} onClick={() => setCurrentPage(page)}>{page}</button>
+
+           }) 
+        }
+
+
+    </div>
+  )
+}
+
+export default Pagination
