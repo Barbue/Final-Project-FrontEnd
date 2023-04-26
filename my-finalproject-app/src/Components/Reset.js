bc0307e84c2113c19eb90f0cdc297b1d7a79f596
Barbue <david.crochet@codeimmersives.com>
2023-04-25 21:38:38 -0500

    modified

---
+++ my-finalproject-app/src/Components/Reset.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Reset = (props) => {
+
+    
+
+
+
+
+return (
+
+    <button onClick={()  => {window.location.reload(true)}}>Reset</button>
+
+    
+
+
+
+
+)
+
+
+};
+
+
+
+
+
+
+
+
+export default Reset
