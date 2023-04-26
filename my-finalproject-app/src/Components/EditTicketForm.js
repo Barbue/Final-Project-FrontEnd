a6d80caaaa99d5ca4e1a72b0df90dc895ea229e8
Barbue <david.crochet@codeimmersives.com>
2023-04-25 21:37:18 -0500

    modified editticketform with userid new fields/labels

---
+++ my-finalproject-app/src/Components/EditTicketForm.js
@@ -0,0 +1,239 @@
+import React from 'react'
+import { useState, useEffect } from "react";
+import axios from 'axios';
+import { useNavigate, useParams  } from 'react-router-dom';
+import { useAuth } from '../Hooks/AuthContext';
+
+function EditTicketForm(props) {
+
+  const { userId } = useAuth();
+
+    const {ticketList, urlEndPoint, setShouldRefresh } = props;  
+    console.log(ticketList)
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [relatedTicketIds, setRelatedTicketIds] = useState(""); //[]
+    const [creator, setCreator] = useState("");
+    const [comments, setComments] = useState("");
+    const [status, setStatus] = useState("");
+    // const [createdById, setCreatedById] = useState("")
+    // const [createdAt, setCreatedAt] = useState("")
+    const [assignedToUserId, setAssignedToUserId] = useState("")
+    // const [lastModified, setLastModified] = useState("")
+    // const [lastUpdatedById, setLastUpdatedById] = useState("")
+    const navigate = useNavigate();
+    const { id } = useParams();
+    console.log(useParams())
+
+    useEffect(() => {
+
+        const foundTicket = ticketList.find((ticket) => {
+          
+        return ticket.createdById === id;
+      });
+       
+        setTitle(foundTicket.title);
+        setDescription(foundTicket.description);
+        setRelatedTicketIds(foundTicket.relatedTicketIds);
+        setAssignedToUserId(foundTicket.assignedToUserId);
+        setCreator(foundTicket.creator);
+        setStatus(foundTicket.status);
+        setComments(foundTicket.comments);
+        // setCreatedById(foundTicket.createdById);
+        // setCreatedAt(foundTicket.createdAt);
+        // setLastModified(foundTicket.lastModified);
+        // setLastUpdatedById(foundTicket.lastUpdatedById);
+
+}, [id, ticketList]);
+
+
+const handleUpdateTicket = () => {
+      
+        setShouldRefresh(true);
+        const req = {
+          title: title,
+          description: description,
+          relatedTicketIds: relatedTicketIds,
+          assignedToUserId: assignedToUserId,
+          creator: creator,
+          status: status,
+          comments: comments,
+          lastModified: new Date(),
+          lastUpdatedById: userId,
+
+    } 
+        const response = axios.put(`${urlEndPoint}/tickets/update-one/${id}`, req)
+        .then(function (response) {
+          console.log(response);
+          setShouldRefresh(false);
+        },{
+        'Content-Type': 'application/json'
+        })
+
+        navigate("/");
+       
+      }
+  
+return (
+
+<div>
+          
+          <h1> Edit Ticket </h1>
+          
+           <label>Title: </label>
+            <input
+                type="text"
+                value={title}
+                name="title"
+                onChange={(e) => { setTitle(e.target.value) }}
+              />
+              <br/>
+              <br/>
+        
+           {/* <p>Description: {ticket.description}</p> */}
+           <label>Description: </label>
+               <textarea
+                type="text"
+                value={description}
+                name="description"
+                onChange={(e) => { setDescription(e.target.value) }}
+              />
+              <br/>
+              <br/>
+
+           {/* <p>relatedTicketsIds: {ticket.relatedTicketsIds}</p> */}
+           <label>relatedTicketIds: </label>
+              <textarea
+              type="text"
+              value={relatedTicketIds}
+              name="relatedTicketIds"
+              onChange={(e) => {
+                setRelatedTicketIds(e.target.value);
+              }}
+            />
+            <br/>
+            <br/>
+
+           <label>assignedToUserId: </label>
+              <input
+              type="text"
+              value={assignedToUserId}
+              name="assignedToUserId"
+              onChange={(e) => {
+                setAssignedToUserId(e.target.value);
+              }}
+            />
+            <br/>
+            <br/>
+
+           {/* <p>Creator: {ticket.creator}</p> */}
+           <label>Creator: </label>
+              <input
+              type="text"
+              value={creator}
+              name="creator"
+              onChange={(e) => { setCreator(e.target.value) }}
+            />
+            <br/>
+            <br/>
+          
+           {/* <p>Status: {ticket.status}</p> */}
+           <label>Status: </label>
+              <input
+                type="text"
+                value={status}
+                name="status"
+                onChange={(e) => {
+                  setStatus(e.target.value);
+                }}
+              />
+              <br/>
+              <br/>
+            
+           {/* <p>Comments: {ticket.comments}</p> */}
+           <label>Comments: </label>
+              <textarea
+                type="text"
+                value={comments}
+                name="comments"
+                onChange={(e) => { setComments(e.target.value) }}
+              />
+              <br/>
+              <br/>
+  
+           {/* <p>Status: {ticket.createdById}</p> */}
+           {/* <label>createdById: </label>
+              <input
+                type="text"
+                value={createdById}
+                name="createdById"
+                onChange={(e) => { setCreatedById(e.target.value) }}
+              /> */}
+              <br/>
+              <br/>
+            
+           {/* <p>Status: {ticket.createdAt}</p> */}
+           {/* <label>createdAt: </label>
+              <input
+                type="datetime-local"
+                value={createdAt}
+                name="createdAt"
+                onChange={(e) => { setCreatedAt(e.target.value) }}
+              /> */}
+              <br/>
+              <br/>
+
+            {/* <label>lastModified: </label>
+              <input
+                type="datetime-local"
+                value={lastModified}
+                name="lastModified"
+                onChange={(e) => { setLastModified(e.target.value) }}
+              /> */}
+              <br/>
+              <br/>
+
+            {/* <label>lastUpdatedById: </label>
+              <input
+                type="text"
+                value={lastUpdatedById}
+                name="lastUpdatedById"
+                onChange={(e) => { setLastUpdatedById(e.target.value) }}
+              /> */}
+              <br/>
+              <br/>
+             
+    <button onClick={() => { handleUpdateTicket() }}
+            > Update Ticket
+            </button>
+            
+             <br/>
+             <br/>
+
+</div>
+);
+}
+  
+
+export default EditTicketForm
+
+
+
+
+
+
+    // title: String,
+    // description: String, 
+    // relatedTicketIds:[String],
+    // assignedToUserId: {type: String, default: uuidv4},
+    // creator: String,
+    // status: String,
+    // comments: String,
+    // createdById: {type: String, default: uuidv4},
+    // createdAt: { type: Date, default: Date.now},
+    // lastModified: { type: Date, default: Date.now},
+    // lastUpdatedById: {type: String, default: uuidv4},
+
+
+     {/* <h2>Title: {ticketList.title}</h2> */}
+      //window.location.reload(true)
