32437212b77b84a547ca88a0b9d705844c3761cb
Barbue <david.crochet@codeimmersives.com>
2023-04-27 14:55:43 -0500

    added react bootstrap button card col row and modal

--- my-finalproject-app/src/Components/TicketCard.js
+++ my-finalproject-app/src/Components/TicketCard.js
@@ -1,13 +1,18 @@
 //import { useState } from "react";
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import QuickEdit from './Modal';
 
 
 const TicketCard = (props) => {
 
   const navigate = useNavigate();
 
-    const {ticket, urlEndPoint, setShouldRefresh } = props;
+    const {ticket, urlEndPoint, setShouldRefresh, ticketList } = props;
     console.log(ticket)
    
 
@@ -28,39 +33,200 @@ const TicketCard = (props) => {
 
 return (
 
-<div>
-
-          <h2>Title: {ticket.title}</h2>
-          <p>Description: {ticket.description}</p>
-          <p>relatedTicketIds: {ticket.relatedTicketIds}</p>
-          <p>AssignedToUserId: {ticket.assignedToUserId}</p>
-          <p>Creator: {ticket.creator}</p>
-          <p>Status: {ticket.status}</p>
-          <p>Comments: {ticket.comments}</p>
-          <p>CreatedAt: {ticket.createdAt}</p>
-          <p>CreatedById: {ticket.createdById}</p>
-          <p>Last Modified: {ticket.lastModified}</p> 
-          <p>LastUpdatedById: {ticket.lastUpdatedById}</p>
-
-       
-<button onClick={() => {handleDeleteTicket(ticket.createdById)
-          }}
-          >Delete Ticket </button>
+  <>
+      {[
+        'Dark',
         
-<button onClick={() => { navigate(`/edit-ticket/${ticket.createdById}`)
-          }}
-          > Edit Ticket
-          </button>
-
-          <br/>
-          <br/>
+      ].map((variant) => (
+        // <CardGroup>
+        <Row xs={1} md={2} className="g-4">
+         
+        {Array.from({ length: 1 }).map((_, idx) => (
+          <Col>
+        <Card border="primary" 
+          bg={variant.toLowerCase()}
+          key={variant}
+          text={variant.toLowerCase() === 'light' ? 'dark' : 'blue'}
+          style={{ width: '27rem' }}
+          className="mb-2"
           
-</div>
+        >
+          <Card.Header>Ticket</Card.Header>
+          <Card.Body>
+
+          <QuickEdit  ticket={ticket} ticketList={ticketList} setShouldRefresh={setShouldRefresh} urlEndPoint={urlEndPoint} />
+          <br/>
+          <br/>
+      <Card.Subtitle style={{fontSize: "25px", fontWeight: 1000}}>  Description: </Card.Subtitle>
+      <Card.Text> <small>  {ticket.description}  </small> </Card.Text>
+
+      <Card.Subtitle style={{fontSize: "25px", fontWeight: 1000}}> RelatedTicketIds: </Card.Subtitle>
+      <Card.Text> <small> {ticket.relatedTicketIds} </small> </Card.Text>
+
+      <Card.Subtitle style={{fontSize: "25px", fontWeight: 1000}}> AssignedToUserId: </Card.Subtitle>
+      <Card.Text> <small> {ticket.assignedToUserId} </small> </Card.Text>
+
+      <Card.Subtitle style={{fontSize: "25px", fontWeight: 1000}}> Creator: </Card.Subtitle>
+      <Card.Text> <small> {ticket.creator} </small> </Card.Text>
+
+      <Card.Subtitle style={{fontSize: "25px", fontWeight: 1000}}> Status: </Card.Subtitle>
+      <Card.Text> <small>  {ticket.status} </small> </Card.Text>
+
+      <Card.Subtitle style={{fontSize: "25px", fontWeight: 1000}}> Comments: </Card.Subtitle>
+      <Card.Text> <small>  {ticket.comments} </small> </Card.Text>
+
+      <Card.Subtitle style={{fontSize: "25px", fontWeight: 1000}}> CreatedAt: </Card.Subtitle>
+      <Card.Text> <small>  {ticket.createdAt} </small> </Card.Text>
+
+      <Card.Subtitle style={{fontSize: "25px", fontWeight: 1000}}> CreatedById: </Card.Subtitle>
+      <Card.Text> <small>  {ticket.createdById} </small> </Card.Text>
+
+      <Card.Subtitle style={{fontSize: "25px", fontWeight: 1000}}> LastUpdatedById: </Card.Subtitle>
+      <Card.Text > <small> {ticket.lastUpdatedById} </small> </Card.Text>
+
+      <Card.Footer>
+        <Card.Subtitle style={{fontSize: "25px", fontWeight: 1000}}> Last Modified: </Card.Subtitle>
+      <small className="text-muted"><p>{ticket.lastModified}</p> </small> 
+        </Card.Footer>
+        
+     
+        <div className="d-grid gap-2">
+            <Button variant="danger" onClick={() => {handleDeleteTicket(ticket.createdById)
+          }}>Delete Ticket</Button>
+         
+            <Button variant="primary" onClick={() => { navigate(`/edit-ticket/${ticket.createdById}`)
+          }}>Edit Ticket</Button>
+        </div>
+          </Card.Body>
+          
+          
+        </Card>
+        </Col>
+      ))}
+    </Row>
+    // </CardGroup>
+      ))}
+    </>
+
+
 );
 }
 
 
 export default TicketCard;
+
+
+
+
+
+
+
+
+   ///* <QuickEdit ticketList={ticketList} setShouldRefresh={setShouldRefresh} /> */
+
+//{/* <Card.Link href="#">Title: {ticket.title} <QuickEdit ticket={ticket} ticketList={ticketList} setShouldRefresh={setShouldRefresh} /></Card.Link> */}
+        //{/* <Card.Title style={{fontSize: "40px", fontWeight: 1000}}><QuickEdit ticket={ticket} ticketList={ticketList} setShouldRefresh={setShouldRefresh} </Card.Title> */}
+
+
+
+//{/* <div>
+
+//  <h2>Title: {ticket.title}</h2>
+// <p>Description: {ticket.description}</p>
+// <p>relatedTicketIds: {ticket.relatedTicketIds}</p>
+// <p>AssignedToUserId: {ticket.assignedToUserId}</p>
+// <p>Creator: {ticket.creator}</p>
+// <p>Status: {ticket.status}</p>
+// <p>Comments: {ticket.comments}</p>
+// <p>CreatedAt: {ticket.createdAt}</p>
+// <p>CreatedById: {ticket.createdById}</p>
+// <p>Last Modified: {ticket.lastModified}</p> 
+// <p>LastUpdatedById: {ticket.lastUpdatedById}</p>
+
+
+// <button onClick={() => {handleDeleteTicket(ticket.createdById)
+// }}
+// >Delete Ticket </button>
+
+// <button onClick={() => { navigate(`/edit-ticket/${ticket.createdById}`)
+// }}
+// > Edit Ticket
+// </button>
+
+// <br/>
+// <br/>
+
+//</div> */
+ //*/}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//<>
+      // {[
+      //   'Primary',
+        
+      // ].map((variant) => (
+      //   <CardGroup>
+      //   <Card
+      //     bg={variant.toLowerCase()}
+      //     key={variant}
+      //     text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
+      //     style={{ width: '18rem' }}
+      //     className="mb-2"
+      //   >
+      //     <Card.Header>Ticket</Card.Header>
+      //     <Card.Body>
+      //       <Card.Title>{variant}Title: {ticket.title}</Card.Title>
+      //       <Card.Text>
+      //     <p>Description: {ticket.description}</p>
+      //     <p>relatedTicketIds: {ticket.relatedTicketIds}</p>
+      //     <p>AssignedToUserId: {ticket.assignedToUserId}</p>
+      //     <p>Creator: {ticket.creator}</p>
+      //     <p>Status: {ticket.status}</p>
+      //     <p>Comments: {ticket.comments}</p>
+      //     <p>CreatedAt: {ticket.createdAt}</p>
+      //     <p>CreatedById: {ticket.createdById}</p>
+          
+      //     <p>LastUpdatedById: {ticket.lastUpdatedById}</p>
+      //       </Card.Text>
+      //       <Button variant="primary" onClick={() => {handleDeleteTicket(ticket.createdById)
+      //     }}>Delete Ticket</Button>
+      //     <Button variant="primary" onClick={() => { navigate(`/edit-ticket/${ticket.createdById}`)
+      //     }}>Edit Ticket</Button>
+      //     </Card.Body>
+      //     <Card.Footer>
+      //     <small className="text-muted"><p>Last Modified: {ticket.lastModified}</p> </small>
+      //   </Card.Footer>
+      //   </Card>
+      //   </CardGroup>
+      // ))}
+    // </> 
+
+
 
 
 
@@ -75,6 +241,16 @@ export default TicketCard;
 
 
  
+
+
+
+
+
+
+
+
+
+
 
 
 
