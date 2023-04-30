674e6d33e57d6b8ca4908175bc4287185a58379a
Barbue <david.crochet@codeimmersives.com>
2023-04-29 22:13:11 -0500

    added form.group for file input

--- my-finalproject-app/src/Components/Modal.js
+++ my-finalproject-app/src/Components/Modal.js
@@ -6,6 +6,9 @@ import axios from 'axios';
 import { useNavigate, useParams  } from 'react-router-dom';
 import { useAuth } from '../Hooks/AuthContext';
 import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import FormGroup from 'react-bootstrap/esm/FormGroup';
+
 
 
 
@@ -15,42 +18,16 @@ function QuickEdit(props) {
 
 const {ticketList, urlEndPoint, setShouldRefresh, ticket,  } = props;  
     
-    const [title, setTitle] = useState(ticket.title); //ticket.title
-    const [description, setDescription] = useState(ticket.description); //ticket.description
-    const [relatedTicketIds, setRelatedTicketIds] = useState(ticket.relatedTicketIds); //ticket.relatedTicketIds
-    const [creator, setCreator] = useState(ticket.creator); //ticket.creator
-    const [comments, setComments] = useState(ticket.comments); //ticket.comments
-    const [status, setStatus] = useState(ticket.status); //ticket.status
-    const [assignedToUserId, setAssignedToUserId] = useState(ticket.assignedToUserId) //ticket.assignedToUserId
+    const [title, setTitle] = useState(ticket.title); 
+    const [description, setDescription] = useState(ticket.description); 
+    const [relatedTicketIds, setRelatedTicketIds] = useState(ticket.relatedTicketIds); 
+    const [creator, setCreator] = useState(ticket.creator); 
+    const [comments, setComments] = useState(ticket.comments); 
+    const [status, setStatus] = useState(ticket.status); 
+    const [assignedToUserId, setAssignedToUserId] = useState(ticket.assignedToUserId) 
     
     const navigate = useNavigate();
-    // const { id } = useParams();
-    // console.log(useParams())
-
-    //  useEffect(() => {
-
-      //   const foundTicket1 = ticket.find((ticket) => {
-            
-          
-      //   return ticket.createdById === id;
-      // });
-      // console.log(foundTicket1)
-      //   setTitle(foundTicket1.title);
-      //   setDescription(foundTicket1.description);
-      //   setRelatedTicketIds(foundTicket1.relatedTicketIds);
-      //   setAssignedToUserId(foundTicket1.assignedToUserId);
-      //   setCreator(foundTicket1.creator);
-      //   setStatus(foundTicket1.status);
-      //   setComments(foundTicket1.comments);
-        // setCreatedById(foundTicket.createdById);
-        // setCreatedAt(foundTicket.createdAt);
-        // setLastModified(foundTicket.lastModified);
-        // setLastUpdatedById(foundTicket.lastUpdatedById);
-
-// }, [id, ticket]);
-
-
-
+    
 const handleUpdateTicket1 = () => {
       
         setShouldRefresh(true);
@@ -80,122 +57,94 @@ const handleUpdateTicket1 = () => {
        
       }
 
-
-
-
-
-
-
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   return (
-    <>
+    <div >
       <Card.Link style={{fontSize: "30px", fontWeight: 1000, color: 'blue'}} onClick={handleShow}>
       Title: {ticket.title}
       </Card.Link>
-
-      <Modal
+      <Modal 
         show={show}
         onHide={handleClose}
         backdrop="static"
         keyboard={false}
-      >
-        <Modal.Header closeButton>
+        
+        >
+        <Modal.Header  closeButton>
           <Modal.Title>Quick Edit</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
+        <Modal.Body >
+          
+
+          
         <h1> Edit Ticket </h1>
           
-          <label>Title: </label>
-           <input
-               type="text"
-               value={title}
-               name="title"
-               onChange={(e) => { setTitle(e.target.value) }}
-             />
-             <br/>
-             <br/>
-        
-          
-           <label>Description: </label>
-              <textarea
-               type="text"
+          <Form>
+          <FormGroup>
+          <Form.Label>Title: </Form.Label>
+          <Form.Control type="text" value={title} name="title" onChange={(e) => { setTitle(e.target.value) }} />
+          </FormGroup>
+          <br/>
+          <FormGroup>
+          <Form.Label>Description: </Form.Label>
+          <Form.Control type="text"
                value={description}
                name="description"
-               onChange={(e) => { setDescription(e.target.value) }}
-             />
+               onChange={(e) => { setDescription(e.target.value) }} as="textarea" rows={3} />
+          </FormGroup>
              <br/>
-             <br/>
-
-           
-           <label>RelatedTicketIds: </label>
-             <textarea
-             type="text"
+          <FormGroup>
+          <Form.Label>RelatedTicketIds: </Form.Label>
+          <Form.Control type="text"
              value={relatedTicketIds}
              name="relatedTicketIds"
              onChange={(e) => {
                setRelatedTicketIds(e.target.value);
-             }}
-           />
+             }} as="textarea" rows={3} />
+          </FormGroup>
            <br/>
-           <br/>
-
-          <label>AssignedToUserId: </label>
-             <input
-             type="text"
-             value={assignedToUserId}
+          <FormGroup>
+          <Form.Label>AssignedToUserId: </Form.Label>
+          <Form.Control type="text" value={assignedToUserId}
              name="assignedToUserId"
              onChange={(e) => {
                setAssignedToUserId(e.target.value);
-             }}
-           />
+             }} as="textarea" rows={3} />
+           </FormGroup>
            <br/>
-           <br/> 
-
-      
-           <label>Creator: </label>
-             <input
-             type="text"
-             value={creator}
+           <FormGroup>
+           <Form.Label>Creator: </Form.Label>
+           <Form.Control type="text" value={creator}
              name="creator"
-             onChange={(e) => { setCreator(e.target.value) }}
-           />
+             onChange={(e) => { setCreator(e.target.value) }} placeholder="Normal text" />
+           </FormGroup>
            <br/>
-           <br/> 
-         
-          
-           <label>Status: </label>
-             <input
-               type="text"
-               value={status}
+           <FormGroup>
+           <Form.Label>Status: </Form.Label>
+           <Form.Control type="text" value={status}
                name="status"
                onChange={(e) => {
                  setStatus(e.target.value);
-               }}
-             />
+               }} placeholder="Normal text" />
+           </FormGroup>
              <br/>
-             <br/>
-            
-        
-           <label>Comments: </label>
-             <textarea
-               type="text"
-               value={comments}
+           <FormGroup>
+           <Form.Label>Comments: </Form.Label>
+           <Form.Control value={comments}
                name="comments"
-               onChange={(e) => { setComments(e.target.value) }}
-             />
-             <br/>
-             <br/>
- 
-          
-  
+               onChange={(e) => { setComments(e.target.value) }} as="textarea" rows={3} />
+            </FormGroup>
+
+            {/* <Form.Group controlId="formFileMultiple" className="mb-3">
+        <Form.Label>Multiple files input example</Form.Label>
+        <Form.Control type="file" multiple />
+      </Form.Group> */}
+            </Form>
            
-           
-            <br/>
-            <br/> 
+            
              
  
         </Modal.Body>
@@ -203,15 +152,147 @@ const handleUpdateTicket1 = () => {
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          {/* <Button variant="primary">Update</Button> */}
+          
           <Button variant="success" onClick={() => {handleUpdateTicket1()
          }}>Update Ticket</Button>  
         </Modal.Footer>
       </Modal>
-    </>
+      </div>
   );
 }
-//handleClose()
+
 
 export default QuickEdit
-//handleUpdateTicket()
+
+
+
+
+
+
+
+{/* <h1> Edit Ticket </h1>
+          
+<label>Title: </label>
+ <input
+     type="text"
+     value={title}
+     name="title"
+     onChange={(e) => { setTitle(e.target.value) }}
+   />
+   <br/>
+   <br/>
+
+
+ <label>Description: </label>
+    <textarea
+     type="text"
+     value={description}
+     name="description"
+     onChange={(e) => { setDescription(e.target.value) }}
+   />
+   <br/>
+   <br/>
+
+ 
+ <label>RelatedTicketIds: </label>
+   <textarea
+   type="text"
+   value={relatedTicketIds}
+   name="relatedTicketIds"
+   onChange={(e) => {
+     setRelatedTicketIds(e.target.value);
+   }}
+ />
+ <br/>
+ <br/>
+
+<label>AssignedToUserId: </label>
+   <input
+   type="text"
+   value={assignedToUserId}
+   name="assignedToUserId"
+   onChange={(e) => {
+     setAssignedToUserId(e.target.value);
+   }}
+ />
+ <br/>
+ <br/> 
+
+
+ <label>Creator: </label>
+   <input
+   type="text"
+   value={creator}
+   name="creator"
+   onChange={(e) => { setCreator(e.target.value) }}
+ />
+ <br/>
+ <br/> 
+
+
+ <label>Status: </label>
+   <input
+     type="text"
+     value={status}
+     name="status"
+     onChange={(e) => {
+       setStatus(e.target.value);
+     }}
+   />
+   <br/>
+   <br/>
+  
+
+ <label>Comments: </label>
+   <textarea
+     type="text"
+     value={comments}
+     name="comments"
+     onChange={(e) => { setComments(e.target.value) }}
+   />
+   <br/>
+   <br/>
+
+
+
+ 
+ 
+  <br/>
+  <br/>  */}
+
+
+
+
+
+
+
+
+
+
+
+//{/* <Button variant="primary">Update</Button> */}
+
+// const { id } = useParams();
+    // console.log(useParams())
+
+    //  useEffect(() => {
+
+      //   const foundTicket1 = ticket.find((ticket) => {
+            
+          
+      //   return ticket.createdById === id;
+      // });
+      // console.log(foundTicket1)
+      //   setTitle(foundTicket1.title);
+      //   setDescription(foundTicket1.description);
+      //   setRelatedTicketIds(foundTicket1.relatedTicketIds);
+      //   setAssignedToUserId(foundTicket1.assignedToUserId);
+      //   setCreator(foundTicket1.creator);
+      //   setStatus(foundTicket1.status);
+      //   setComments(foundTicket1.comments);
+        // setCreatedById(foundTicket.createdById);
+        // setCreatedAt(foundTicket.createdAt);
+        // setLastModified(foundTicket.lastModified);
+        // setLastUpdatedById(foundTicket.lastUpdatedById);
+
+// }, [id, ticket]);
