03c6d47e9d740c6f5d06487af33dd075ec930951
Barbue <david.crochet@codeimmersives.com>
2023-04-29 22:16:11 -0500

    added react icons, changed logoutbutton to card.link, added css styling

--- my-finalproject-app/src/Components/NavBar.js
+++ my-finalproject-app/src/Components/NavBar.js
@@ -3,6 +3,10 @@ import { useAuth } from "../Hooks/AuthContext";
 import { removeUserToken } from "../Hooks/authLocalStorage";
 import { useNavigate } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { VscSignIn, VscSignOut, VscSaveAs } from 'react-icons/vsc';
+import { FcHome } from "react-icons/fc";
+import { FaUserEdit } from "react-icons/fa";
 
 const NavBar = () => {
     
@@ -17,18 +21,21 @@ const NavBar = () => {
 
 return (
 
-          <nav> 
+          <nav className="navbar"> 
 
-           <Link className="link" to="/"> Home </Link>   
+           <Link className="link" to="/"><FcHome /> Home </Link>  
+           {" "}{" "} 
            
-           {isVerified && <Link className="link2" to="/ticketform"> Create Ticket </Link> }
+           {isVerified && <Link className="link2" to="/ticketform"> Create Ticket <VscSaveAs /> </Link> }
+           {" "}
 
-         {isVerified && <Link className="link3" to="/registration"> Register </Link>}
+         {isVerified && <Link className="link3" to="/registration"> Register <FaUserEdit/> </Link>}
 
-          <Link className="link4" to="/login"> Login </Link> 
+          <Link className="link4" to="/login"> Login < VscSignIn /></Link> 
+          {""}
           
-          
-         {isVerified && <Button variant="primary" size="sm" onClick={handleLogout}>Logout</Button>}
+         {isVerified && <Card.Link  style={{color: 'green', fontFamily:'Gill Sans, Gill Sans MT, Calibri, Trebuchet MS, sans-serif', fontWeight:1000
+        }}variant="primary" size="sm" onClick={handleLogout}>Logout <VscSignOut /></Card.Link>}
 
          
           </nav> 
