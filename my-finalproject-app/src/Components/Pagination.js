f12f38704accbdaff9a2f23c6e196a2134684e37
Barbue <david.crochet@codeimmersives.com>
2023-04-29 22:17:25 -0500

    added classname to button

--- my-finalproject-app/src/Components/Pagination.js
+++ my-finalproject-app/src/Components/Pagination.js
@@ -12,11 +12,11 @@ const Pagination = ({totalTickets, ticketsPerPage, setCurrentPage}) => {
     
 
   return (
-    <div>
+    <div className="pages">
         {
            pages.map((page, i) => {
 
-           return <Button variant="success"  key={i} onClick={() => {setCurrentPage(page)
+           return <Button className="pages1" variant="success"  key={i} onClick={() => {setCurrentPage(page)
            }}>{page}</Button> 
 
            
@@ -30,6 +30,42 @@ const Pagination = ({totalTickets, ticketsPerPage, setCurrentPage}) => {
 }
 
 export default Pagination
+
+
+
+
+// import Pagination from 'react-bootstrap/Pagination';
+
+// let active = 2;
+// let items = [];
+// for (let number = 1; number <= 5; number++) {
+//   items.push(
+//     <Pagination.Item key={number} active={number === active}>
+//       {number}
+//     </Pagination.Item>,
+//   );
+// }
+
+// const paginationBasic = (
+//   <div>
+//     <Pagination>{items}</Pagination>
+//     <br />
+
+//     <Pagination size="lg">{items}</Pagination>
+//     <br />
+
+//     <Pagination size="sm">{items}</Pagination>
+//   </div>
+// );
+
+
+
+
+
+
+
+
+
 
 
 
